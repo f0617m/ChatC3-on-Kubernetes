b40acce1d5947a5eba1689abd3d94dbf2332d572
takana <taizo0233@gmail.com>
2020-05-12 16:38:12 +0900

    rubocop_todo.yamlの修正3

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,40 +1,32 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     stream_from "start_#{params['room_id']}"
     stream_from "messages_#{params['room_id']}"
     stream_from "lines_#{params['room_id']}"
     stream_from "remove_#{params['room_id']}"
 
-    p '▽サブスクライブド▽'
     @user = User.find_by(user_id: params['user_id'])
 
     if @user
       count = User.where(room_id: params['room_id']).count
 
       @room = Room.find_by(id: params['room_id'])
-      if count == 0
+
+      case count
+      when 0 then
         @room.status = 'Finished'
-        p 'Finished'
-      elsif count == 1
+      when 1 then
         @room.status = 'Waiting'
-        p 'Waiting'
-      elsif count == 2
+      when 2 then
         @room.status = 'Plaing'
-        p 'Playing'
-        startChat()
+        startChat
       end
 
       @room.save
     end
-    p '△サブスクライブド△'
-
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-    p '▽アンサブスクライブド▽'
-
     @user = User.find_by(user_id: params['user_id'])
     @user.room_id = nil
     @user.save
@@ -42,30 +34,25 @@ class RoomChannel < ApplicationCable::Channel
     count = User.where(room_id: params['room_id']).count
 
     @room = Room.find_by(id: params['room_id'])
-    if count == 0
+
+    case count
+    when 0 then
       @room.status = 'Finished'
-      p 'Finished'
-    elsif count == 1
+    when 1 then
       @room.status = 'Finished'
-      p 'Finished'
-      removeRoom()
-    elsif count == 2
-      @room.status = 'Playing'
-      p 'Playing'
+      removeRoom
+    when 2 then
+      @room.status = 'Plaing'
     end
-
+ 
     @room.save
-
-    p '△アンサブスクライブド△'
   end
 
   def startChat
-    ActionCable.server.broadcast "start_#{params['room_id']}",
-    {action: 'start'}
+    ActionCable.server.broadcast "start_#{params['room_id']}", { action: 'start' }
   end
 
   def removeRoom
-    ActionCable.server.broadcast "remove_#{params['room_id']}",
-    {action: 'remove'}
+    ActionCable.server.broadcast "remove_#{params['room_id']}", { action: 'remove' }
   end
 end
