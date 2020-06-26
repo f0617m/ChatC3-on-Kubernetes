4045083e9bcc60365069cc2f46b622706b60a6ce
takana <taizo0233@gmail.com>
2020-06-26 22:05:23 +0900

    rubocop

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -86,11 +86,12 @@ module Api
       def authenticate
         authenticate_or_request_with_http_token do |token, options|
           auth_user = User.find_by(token: token)
-          auth_user != nil ? true : false
+          auth_user != nil
         end
       end
 
       private
+
         def set_message
           @message = Message.find(params[:id])
         end
