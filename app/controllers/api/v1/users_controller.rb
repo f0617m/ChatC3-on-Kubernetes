b328e6a366c4dcb32ce0ef30ef493cd920be6be9
takana <taizo0233@gmail.com>
2020-06-26 23:46:51 +0900

    rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -147,7 +147,7 @@ module Api
       end
 
       def authenticate
-        authenticate_or_request_with_http_token do |token,options|
+        authenticate_or_request_with_http_token do |token, options|
           auth_user = User.find_by(token: token)
           auth_user != nil
         end
