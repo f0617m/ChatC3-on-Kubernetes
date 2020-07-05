86912923ea78e499db8c7a6c244b9c3d4eddd697
takana <taizo0233@gmail.com>
2020-07-05 16:04:08 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token
 
-  validates :user_id, presence: true, uniqueness: {message: 'このユーザーIDは既に使用されています'}
+  validates :user_id, presence: true, uniqueness: {message: 'このユーザーIDは既に使用されています' }
   validates :name, presence: true
   validates :image_name, presence: true
   validates :password_digest, presence: true
