f317cd2666395a529a7b9e3c664414d248a3a23d
takana <taizo0233@gmail.com>
2020-07-05 23:43:29 +0900

    rspec messages

---
+++ spec/controllers/api/v1/messages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+
+describe 'PostAPI' do
+    it 'getMessagesでMessagesを取得する' do
+
+        get "/api/v1/messages/getMessages"
+        json = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+    end
+end
