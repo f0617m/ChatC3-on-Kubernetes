8cfa1926ed9450641cf41d87cf60463aa4adbf6c
takana <taizo0233@gmail.com>
2020-07-06 07:29:09 +0900

    rspec messages

--- spec/controllers/api/v1/messages_spec.rb
+++ spec/controllers/api/v1/messages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "MessagesController" do
     it 'getMessagesでMessagesを取得する' do
 
-        get "/api/v1/messages/getMessages"
+        get "/api/v1/messages/getMessages/1"
         json = JSON.parse(response.body)
 
         expect(response.status).to eq(200)
