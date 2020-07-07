b55f0394233737be0ab6c972dd9dbc1eb748aa1b
takana <taizo0233@gmail.com>
2020-07-08 00:55:46 +0900

    rspec rooms

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -4,13 +4,14 @@ module AuthenticationHelper
   extend ActiveSupport::Concern
   
   self.included do
+    describe 'self test' do
+      it 'self test2' do
+        expect(self).to eq 'aiueo'
+      end
+    end
+
     before do
       controller_mock = double('Controller mock')
-      it 'self test' do
-        expect(self).to eq 'aiueo'
-
-      end
-
       allow(controller_mock).to receive(:authenticate).and_return(true)
 
       allow(self).to receive(:authenticate).and_return(controller_mock)
