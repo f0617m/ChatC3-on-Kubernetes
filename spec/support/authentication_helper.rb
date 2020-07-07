6032034012bd9181e054abc777972904bfb033ae
takana <taizo0233@gmail.com>
2020-07-07 23:08:47 +0900

    rspec rooms

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -4,13 +4,9 @@ module AuthenticationHelper
   self.included do
     before do
       controller_mock = double('Controller mock')
-      allow(controller_mock).to receive(:authenticate)
+      allow(controller_mock).to receive(:authenticate).and_return(true)
 
-      allow(self).to receive(controller_mock)
+      allow(self).to receive(:authenticate).and_return(controller_mock)
     end
-  end
-
-  def authenticate
-    return true
   end
 end
