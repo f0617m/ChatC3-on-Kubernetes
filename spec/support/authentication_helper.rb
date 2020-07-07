7e0894056926713c7412b859bfc97d72816cf580
takana <taizo0233@gmail.com>
2020-07-08 00:46:38 +0900

    rspec rooms

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -6,7 +6,7 @@ module AuthenticationHelper
       controller_mock = double('Controller mock')
       allow(controller_mock).to receive(:authenticate).and_return(true)
 
-      allow(RoomsController).to receive(:authenticate).and_return(controller_mock)
+      allow(App::Controllers::Api::V1::RoomsController).to receive(:authenticate).and_return(controller_mock)
     end
   end
 end
