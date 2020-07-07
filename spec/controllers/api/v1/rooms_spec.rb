eafeec319e0cefc8cfefa99de96a0cd5e155be31
takana <taizo0233@gmail.com>
2020-07-08 00:30:15 +0900

    rspec rooms

--- spec/controllers/api/v1/rooms_spec.rb
+++ spec/controllers/api/v1/rooms_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Api::V1::RoomsController, type: :controller, authentication: :skip do
+
+  it 'test' do
+    expect(200).to eq 200
+  end
+
   it 'res code 200' do
     get 'find'
     expect(response.status).to eq 200
