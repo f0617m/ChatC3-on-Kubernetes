a94ed4965c97da3108c5029301e19bca980e378c
takana <taizo0233@gmail.com>
2020-07-07 22:50:04 +0900

    rspec rooms

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -3,7 +3,11 @@ module AuthenticationHelper
   
   self.included do
     before do
-      allow(self).to receive(:authenticate).and_return(true)
+      allow(self).to receive(:authenticate)
     end
+  end
+
+  def authenticate
+    return true
   end
 end
