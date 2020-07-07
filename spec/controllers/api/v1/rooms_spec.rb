5e232a278eede0985ab5e7e9cc970bc1dc8e643a
takana <taizo0233@gmail.com>
2020-07-07 19:34:39 +0900

    rspec rooms

--- spec/controllers/api/v1/rooms_spec.rb
+++ spec/controllers/api/v1/rooms_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require 'spec_helper'
 
 describe 'RoomsController' do
     it 'res code 200' do
-        get "/api/v1/getRoom"
+        get '/api/v1/getRoom'
         expect(response.status).to eq 200
         json = JSON.parse(response.body)
     end
