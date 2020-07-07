5ad5b68754d36930a3579014a753d0acfc7b1ec4
takana <taizo0233@gmail.com>
2020-07-07 23:33:10 +0900

    rspec rooms

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -4,13 +4,17 @@ module AuthenticationHelper
   self.included do
     before do
       controller_mock = double('Controller mock')
-      allow(controller_mock).to receive(:authenticate).and_return(true)
+      allow(controller_mock).to receive(:authenticate_or_request_with_http_token).and_return(true)
 
       allow(self).to receive(:authenticate).and_return(controller_mock)
     end
   end
 
   def authenticate
+    return true
+  end
+
+  def authenticate_or_request_with_http_token
     return true
   end
 end
