26c2de972a134a368d33d231a9910a3d1167d17f
takana <taizo0233@gmail.com>
2020-07-08 00:15:08 +0900

    rspec rooms

--- spec/controllers/api/v1/rooms_spec.rb
+++ spec/controllers/api/v1/rooms_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::RoomsController, type: :controller, authentication: :skip do
   it 'res code 200' do
     authorization = ActionController::HttpAuthentication::Token.encode_credentials('token')
-    request.headers.merge!({ 'HTTP_AUTHORIZATION' => authorization })
+    request.headers.merge!(HTTP_AUTHORIZATION: authorization)
     get 'find'
     expect(response.status).to eq 200
     json = JSON.parse(response.body)
