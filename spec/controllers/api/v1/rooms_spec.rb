227de467ffd826c2d9028bde7ee433864fcae7fd
takana <taizo0233@gmail.com>
2020-07-08 10:15:20 +0900

    rspec rooms

--- spec/controllers/api/v1/rooms_spec.rb
+++ spec/controllers/api/v1/rooms_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::V1::RoomsController, type: :controller, authentication: :skip do
-  before { authenticateMock(Api::V1::RoomsController) }
+  before { authenticateMock(new Api::V1::RoomsController) }
 
   it 'test' do
     expect(200).to eq 200
