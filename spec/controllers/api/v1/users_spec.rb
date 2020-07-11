906b17236ce11bb438c38bf6498a95cf69d23a44
takana <taizo0233@gmail.com>
2020-07-11 17:59:32 +0900

    rspec

--- spec/controllers/api/v1/users_spec.rb
+++ spec/controllers/api/v1/users_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :controller, authentication: :skip do
-  before { authenticateMock(controller) }
-
-  room = create(:user)
-  post 'create', params: { user_id: user.user_id, name: user.name, image_name: user.image_name, password: user.password }
-
+  before do
+    authenticateMock(controller)
+    room = create(:user)
+    post 'create', params: { user_id: user.user_id, name: user.name, image_name: user.image_name, password: user.password }
+  end
+  
   it 'Get /getroom' do
   end
 end
