79e62ccb1df70126f6a1422821de5415d802bea6
takana <taizo0233@gmail.com>
2020-07-11 17:18:41 +0900

    rspec

--- spec/support/testdata_helper.rb
+++ spec/support/testdata_helper.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'messages_spec'
 
 module TestDataHelper
 
@@ -8,7 +7,7 @@ module TestDataHelper
     # テストデータの登録
     # user
     user = create(:user)
-    post "/api/v1/users/", params: { user_id: user.user_id, name: user.name, image_name: user.image_name, password: user.password }
+    post "/api/v1/users/aaaaa/aaa", params: { user_id: user.user_id, name: user.name, image_name: user.image_name, password: user.password }
     # post "/api/v1/users/", :user_id => user.user_id, :name => user.name, :image_name => user.image_name, :password => user.password
 
     # room
