991c20bace9df608eb4fb080974b7186494f50c3
takana <taizo0233@gmail.com>
2020-07-11 17:11:15 +0900

    rspec

--- spec/support/testdata_helper.rb
+++ spec/support/testdata_helper.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'messages_controller'
 
 module TestDataHelper
 
@@ -12,10 +13,10 @@ module TestDataHelper
 
     # room
     room = create(:room)
-    post "/api/v1/room/", :status => room.status
+    post "/api/v1/rooms/", :status => room.status
 
     # message
     message = create(:message)
-    post "/api/v1/message/", :message => message.message, :user_id => message.user_id, :room_id => message.room_id
+    post "/api/v1/messages/", :message => message.message, :user_id => message.user_id, :room_id => message.room_id
   end
 end
