97de1d1034ec2f05015bc28f66bafad407cdf0e8
takana <taizo0233@gmail.com>
2020-07-11 18:06:15 +0900

    rspec

--- spec/controllers/api/v1/messages_spec.rb
+++ spec/controllers/api/v1/messages_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe Api::V1::MessagesController, type: :controller, authentication: :skip do
-  before { authenticateMock(controller) }
-
-  let(:message) { create(:message) }
-  post 'create', params: { message: message.message, user_id: message.user_id, room_id: message.room_id }
+  before do
+    authenticateMock(controller)
+    message = create(:message) 
+    post 'create', params: { message: message.message, user_id: message.user_id, room_id: message.room_id }
+  end
 
   it 'GET /getMessages/:id' do
     get 'getMessages', :id => messages.room_id
--- spec/controllers/api/v1/rooms_spec.rb
+++ spec/controllers/api/v1/rooms_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe Api::V1::RoomsController, type: :controller, authentication: :skip do
-  before { authenticateMock(controller) }
-
-  room = create(:room)
-  post 'create', params: { status: room.status }
+  before do
+    authenticateMock(controller)
+    room = create(:room)
+    post 'create', params: { status: room.status }
+  end
 
   it 'Get /getroom' do
     get 'find'
